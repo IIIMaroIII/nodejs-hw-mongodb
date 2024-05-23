161dd32d616e622884b6ace40df54d3952bb147d
Rodion Matvieiev <your.email@example.com>
2024-05-22 23:26:15 -0400

    updated by zsh func

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
-console.log('first time');
+import { setupServer } from './server.js';
+
+setupServer();
--- src/server.js
+++ src/server.js
@@ -1,8 +1,26 @@
 import express from 'express';
 import cors from 'cors';
+import { envPort } from './utils/envPort.js';
+import { ENV_VARS } from './constants/constants.js';
 
 export const setupServer = () => {
   const app = express();
 
   app.use(cors());
+  app.use(express.json());
+
+  app.get('/', (req, res, next) => {
+    res.send('Hello world');
+  });
+
+  app.use((req, res, next) => {
+    res.status(404).json({
+      message: 'Page wasn`t found!',
+    });
+  });
+
+  app.listen(
+    envPort(ENV_VARS.PORT, 3000),
+    console.log(`Server is running on ${process.env[ENV_VARS.PORT]}`),
+  );
 };
