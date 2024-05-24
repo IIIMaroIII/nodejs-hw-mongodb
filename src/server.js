365440f490789acf4ef2ec50c1cf0d710dcca6e9
Rodion Matvieiev <your.email@example.com>
2024-05-23 20:34:43 -0400

    updated by zsh func

--- src/server.js
+++ src/server.js
@@ -2,20 +2,22 @@ import express from 'express';
 import cors from 'cors';
 import { envPort } from './utils/envPort.js';
 import { ENV_VARS } from './constants/constants.js';
+import { logger } from './utils/pino.js';
 
 export const setupServer = () => {
   const app = express();
 
+  app.use(logger());
   app.use(cors());
   app.use(express.json());
 
-  app.get('/', (req, res, next) => {
+  app.get('/', (req, res) => {
     res.send('Hello world');
   });
 
-  app.use((req, res, next) => {
+  app.use((req, res) => {
     res.status(404).json({
-      message: 'Page wasn`t found!',
+      message: 'Not found!',
     });
   });
 
