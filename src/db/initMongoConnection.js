c8976a0442834e7235e94c164a3d63aa6d8a4d0a
Rodion Matvieiev <your.email@example.com>
2024-05-26 12:48:05 -0400

    updated

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -11,7 +11,7 @@ export const initMongoConnection = async () => {
   )}?retryWrites=true&w=majority&appName=Cluster0`;
   try {
     await mongoose.connect(connectionLink);
-    console.log('Successfully database connection');
+    console.log(`Successfully connected to ${env(MONGODB_DB)}`);
   } catch (error) {
     throw new Error(error);
   }
