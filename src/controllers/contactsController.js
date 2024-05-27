ddf405109c8e32d2034ee468e13cc845315cab08
Rodion Matvieiev <your.email@example.com>
2024-05-27 00:08:39 -0400

    updated

---
+++ src/controllers/contactsController.js
@@ -0,0 +1,44 @@
+import { getAllContacts, getContactById } from '../services/contacts.js';
+import mongoose from 'mongoose';
+import { HttpError } from '../utils/HttpError.js';
+
+export const homePage = (req, res, next) => {
+  try {
+    res.json({
+      status: '200',
+      message: 'Hello, this is my first backend`s answer!',
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+export const contactsPage = async (req, res, next) => {
+  try {
+    const result = await getAllContacts();
+    res.json({
+      status: '200',
+      message: 'Successfully found contacts!',
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const contactById = async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(contactId)) {
+      throw HttpError(404, `The contact with ${contactId} was not found!`);
+    }
+    const result = await getContactById(contactId);
+    res.json({
+      status: '200',
+      message: `Successfully found contact with ${contactId}}!`,
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
--- src/routes/api/contactsRoutes.js
+++ src/routes/api/contactsRoutes.js
@@ -1,48 +1,15 @@
 import express from 'express';
-import { getAllContacts, getContactById } from '../../services/contacts.js';
-import mongoose from 'mongoose';
-import { HttpError } from '../../utils/HttpError.js';
+
+import {
+  contactById,
+  contactsPage,
+  homePage,
+} from '../../controllers/contactsController.js';
 
 export const contactsRouters = express.Router();
 
-contactsRouters.get('/', (req, res, next) => {
-  try {
-    res.json({
-      status: '200',
-      message: 'Hello, this is my first backend`s answer!',
-    });
-  } catch (error) {
-    next(error);
-  }
-});
+contactsRouters.get('/', homePage);
 
-contactsRouters.get('/contacts', async (req, res, next) => {
-  try {
-    const result = await getAllContacts();
-    res.json({
-      status: '200',
-      message: 'Successfully found contacts!',
-      data: result,
-    });
-  } catch (error) {
-    next(error);
-  }
-});
+contactsRouters.get('/contacts', contactsPage);
 
-contactsRouters.get('/contacts/:contactId', async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-
-    if (!mongoose.Types.ObjectId.isValid(contactId)) {
-      throw HttpError(404, `The contact with ${contactId} was not found!`);
-    }
-    const result = await getContactById(contactId);
-    res.json({
-      status: '200',
-      message: `Successfully found contact with ${contactId}}!`,
-      data: result,
-    });
-  } catch (error) {
-    next(error);
-  }
-});
+contactsRouters.get('/contacts/:contactId', contactById);
