f3974b88c63e6b3edfa95645b95eabec7fbf5137
Rodion Matvieiev <your.email@example.com>
2024-05-27 23:53:21 -0400

    updated

--- src/controllers/contactsController.js
+++ src/controllers/contactsController.js
@@ -1,21 +1,15 @@
 import mongoose from 'mongoose';
 import { HttpError } from '../utils/HttpError.js';
 import { Collection } from '../services/contacts.js';
+import { ResponseMaker } from '../utils/responseMaker.js';
 
 const homePage = (req, res) => {
-  res.json({
-    status: '200',
-    message: 'Hello, this is my first backend`s answer!',
-  });
+  res.json(ResponseMaker(200, 'Hello, this is my first backend`s answer!'));
 };
 
 const contactsPage = async (req, res) => {
   const result = await Collection.getAllContacts();
-  res.json({
-    status: '200',
-    message: 'Successfully found contacts!',
-    data: result,
-  });
+  res.json(ResponseMaker(200, 'Successfully found contacts!', result));
 };
 
 const contactById = async (req, res, next) => {
@@ -29,11 +23,13 @@ const contactById = async (req, res, next) => {
   if (!result) {
     return next(HttpError(404, `The contact with ${contactId} was not found!`));
   }
-  res.json({
-    status: '200',
-    message: `Successfully found contact with ${contactId}}!`,
-    data: result,
-  });
+  res.json(
+    ResponseMaker(
+      200,
+      `Successfully found contact with ${contactId}}!`,
+      result,
+    ),
+  );
 };
 
 export const ctrl = {
