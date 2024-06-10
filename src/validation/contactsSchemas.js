7ef4589adb60b15717e93f3acba4beae952baada
Rodion Matvieiev <your.email@example.com>
2024-06-10 00:17:11 -0400

    update

---
+++ src/validation/contactsSchemas.js
@@ -0,0 +1,34 @@
+import joi from 'joi';
+
+const addNewContactSchema = joi.object({
+  name: joi.string().min(3).max(20).required().messages({
+    'string.base': '"name" should be a type of text',
+    'string.empty': '"name" cannot be an empty field',
+    'string.min': '"name" should have a minimum length of {#limit}',
+    'string.max': '"name" should have a maximum length of {#limit}',
+    'any.required': '"name" is a required field',
+  }),
+  phoneNumber: joi.string().min(3).max(20).required().messages({
+    'string.base': '"phoneNumber" should be a type of text',
+    'string.empty': '"phoneNumber" cannot be an empty field',
+    'string.min': '"phoneNumber" should have a minimum length of {#limit}',
+    'string.max': '"phoneNumber" should have a maximum length of {#limit}',
+    'any.required': '"phoneNumber" is a required field',
+  }),
+  email: joi.string().min(3).max(20).email().messages({
+    'string.base': '"email" should be a type of text',
+    'string.empty': '"email" cannot be an empty field',
+    'string.min': '"email" should have a minimum length of {#limit}',
+    'string.max': '"email" should have a maximum length of {#limit}',
+    'string.email': '"email" must be a valid email address',
+  }),
+  isFavourite: joi.boolean().default(false).messages({
+    'boolean.base': '"isFavourite" should be a boolean value',
+  }),
+});
+
+const patchContactSchema = joi.object({});
+
+export const contactsSchemas = {
+  addNewContactSchema,
+};
